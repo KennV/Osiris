0821abd42ccc690de060688a0440863135d20345
Kenn Villegas <kenn.@phoenix.64.local>
2017-06-20 13:40:52 -0400

    ADDED ITEM AND ENTITY ABSTRACT CONTROLLERS
    Actually DataControllers Just Abstract

---
+++ Osiris/Data/Controllers/Abstract/KVEntityDataController.swift
@@ -0,0 +1,19 @@
+/**
+  KVEntityDataController.swift
+  Osiris
+
+  Created by Kenn Villegas on 6/20/17.
+  Copyright © 2017 dubian. All rights reserved.
+
+  Entities are Named World Objects. People Places and Things if you will;
+  These are seperate from Items which are Goods and Services
+  this is still one Level Abstracted from Person and Vendor
+
+*/
+
+import UIKit
+
+class KVEntityDataController<T : KVEntity > : KVAbstractDataController<T>
+{
+
+}
---
+++ Osiris/Data/Controllers/Abstract/KVItemDataController.swift
@@ -0,0 +1,19 @@
+/**
+  KVItemDataController.swift
+  Osiris
+
+  Created by Kenn Villegas on 6/20/17.
+  Copyright © 2017 dubian. All rights reserved.
+
+  Items are Named World Objects. which are Goods and Services if you will;
+  These are seperate from Entities are People Places and Things
+  this is still one Level Abstracted from goods and services
+
+*/
+
+import UIKit
+
+class KVItemDataController<T : KVItem > : KVAbstractDataController<T>
+{
+
+}
