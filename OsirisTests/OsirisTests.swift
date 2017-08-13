dbdb01572b9d34c8d6c2594d645cf7fbaf369dcc
Kenn Villegas <kenn.@phoenix.64.local>
2017-08-13 17:41:02 -0400

    REFACTORED NAMES AND SCOPE
    For the variables in this test module
    it eliminates all of the repetition

--- OsirisTests/OsirisTests.swift
+++ OsirisTests/OsirisTests.swift
@@ -12,8 +12,9 @@ import CoreData
 @testable import Osiris
 
 class OsirisTests: XCTestCase {
-  var SUT_PSK : NSPersistentContainer? = nil
-  var MOC : NSManagedObjectContext? = nil
+  var PSK : NSPersistentContainer? = nil
+  var testMOC : NSManagedObjectContext? = nil
+  var SUT = KVPrimeTVController()
 
   func setupPSK() -> NSPersistentContainer
   {
@@ -37,132 +38,118 @@ class OsirisTests: XCTestCase {
       }
     }
     )
-    self.MOC = container.viewContext
+    testMOC = container.viewContext
     return container
   }
   override func setUp()
   {
     super.setUp()
-    SUT_PSK = setupPSK()
+    PSK = setupPSK()
+    SUT.AllDataController.PSK = PSK!
+    SUT.AllDataController.MOC = PSK!.viewContext
+    SUT.personDataController.PSK = SUT.AllDataController.PSK
+    SUT.personDataController.MOC = SUT.AllDataController.MOC
+    SUT.vendorDataController.PSK = SUT.AllDataController.PSK
+    SUT.vendorDataController.MOC = SUT.AllDataController.MOC
+    SUT.sessionDataController.PSK = SUT.AllDataController.PSK
+    SUT.sessionDataController.MOC = SUT.AllDataController.MOC
   }
   override func tearDown()
   {
-    MOC = nil
-    SUT_PSK = nil
+    testMOC = nil
+    PSK = nil
     super.tearDown()
+  }
+  func testADController()
+  {
+    XCTAssertNotNil(KVOsirisDataController(ctx: (PSK?.viewContext)!), "Combi")
+    
+//    let vue = KVPrimeTVController()
+    XCTAssertNotNil(SUT.AllDataController.MOC, "No MOC")
+    
+    SUT.personDataController.MOC = SUT.AllDataController.MOC
+    _ = SUT.AllDataController.createEntityInContext((PSK?.viewContext)!, type: EntityTypes.RootEntity)
+    
+    SUT.personDataController.makePerson()
+    
+    XCTAssertNotNil(SUT.AllDataController.makeRandomNumber(2))
+    XCTAssertNotNil(SUT.AllDataController.makeRandomNumberCurve(2, 6))
+    XCTAssertNotNil(SUT.AllDataController.makeRandomPhoneNumber())
+    XCTAssertNotNil(SUT.AllDataController.makeRandomHexQuad())
   }
   func testEntityController()
   {
     let testEDC = KVEntityDataController<KVEntity>()
-    testEDC.PSK = self.SUT_PSK!
+    testEDC.PSK = PSK!
     let entityTestItem = testEDC.createEntityInContext(testEDC.PSK.viewContext, type: EntityTypes.Entity)
     
     XCTAssertNotNil((testEDC), "Failed to Create Entity Controller")
-    XCTAssertNotNil(testEDC.PSK.viewContext, "Failed to Create MOC")
+    XCTAssertNotNil(testEDC.MOC, "Failed to Create MOC")
     XCTAssertNotNil(entityTestItem, "Failed to Create entity")
-  }
-  func testADController()
-  {
-    XCTAssertNotNil(KVOsirisDataController(ctx: (SUT_PSK?.viewContext)!), "Combi")
-    
-    let pTVC = KVPrimeTVController()
-    pTVC.AllDataController.PSK = SUT_PSK!
-    
-    XCTAssertNotNil(pTVC.AllDataController.MOC, "No MOC")
-    pTVC.personDataController.MOC = pTVC.AllDataController.MOC
-
-    XCTAssertEqual(0, pTVC.AllDataController.getAllEntities().count)
-    _ = pTVC.AllDataController.createEntityInContext((SUT_PSK?.viewContext)!, type: EntityTypes.RootEntity)
-//    XCTAssertEqual(1, pTVC.AllDataController.getAllEntities().count)
-    
-//    XCTAssertEqual(0, pTVC.people.count)
-    pTVC.personDataController.makePerson()
-//    XCTAssertEqual(1, pTVC.people.count)
-    
-    XCTAssertNotNil(pTVC.AllDataController.makeRandomNumber(2))
-    XCTAssertNotNil(pTVC.AllDataController.makeRandomNumberCurve(2, 6))
-    XCTAssertNotNil(pTVC.AllDataController.makeRandomPhoneNumber())
-    XCTAssertNotNil(pTVC.AllDataController.makeRandomHexQuad())
   }
   func testPDController()
   {
-    let TVC = KVPrimeTVController()
-    
-    TVC.AllDataController.PSK = SUT_PSK!
-    XCTAssertNotNil(TVC.AllDataController.MOC, "No MOC")
-    
-    TVC.personDataController.MOC = TVC.AllDataController.MOC
-//    XCTAssertEqual(0, TVC.people.count)
-    TVC.personDataController.makePerson()
-//    XCTAssertEqual(1, TVC.people.count)
+//    let vue = KVPrimeTVController()
+    XCTAssertNotNil(SUT.AllDataController.MOC, "No MOC")
+    XCTAssertNotNil(SUT.people, "No ppl arr")
+    SUT.personDataController.makePerson()
     // Currently only testing the one ivar from graphics, location and physics
-    let joe = TVC.people[0]
+    let joe = SUT.people[0]
     XCTAssertEqual("New", joe.graphics?.caption)
     XCTAssertEqual("NewAddress", joe.location?.address)
     XCTAssertEqual(100, joe.physics?.massKG)
-    //
+
     XCTAssertEqual(joe, joe.graphics?.owner)
     XCTAssertEqual(joe, joe.location?.owner)
     XCTAssertEqual(joe, joe.physics?.owner)
   }
   func testVendorCon()
   {
-    let vue = KVPrimeTVController()
-    let testVendorDataController = vue.vendorDataController
-    testVendorDataController.PSK = SUT_PSK!
-    testVendorDataController.MOC = SUT_PSK?.viewContext
-    
-    XCTAssertEqual(0, testVendorDataController.getAllEntities().count)
+    SUT.vendorDataController.PSK = PSK!
+    SUT.vendorDataController.MOC = PSK?.viewContext
+    XCTAssertNotNil(SUT.vendors, "Nil vndr arr")
+    XCTAssertEqual(0, SUT.vendorDataController.getAllEntities().count)
 //    let tj = testVendorDataController.createEntityInContext((testVendorDataController.MOC)!, type: EntityTypes.Vendor)
-    let tj = testVendorDataController.createVendorInContext(testVendorDataController.MOC!)
+    let tj = SUT.vendorDataController.createVendorInContext(SUT.vendorDataController.MOC!)
     XCTAssertNotNil(tj)
     
-    let testVendor = testVendorDataController.getAllEntities()[0]
+    let testVendor = SUT.vendorDataController.getAllEntities()[0]
     XCTAssertNotNil(testVendor)
-    XCTAssertEqual(1, testVendorDataController.getAllEntities().count)
+    XCTAssertEqual(1, SUT.vendorDataController.getAllEntities().count)
     //
-    testVendorDataController.deleteEntityInContext((testVendorDataController.MOC)!, entity: testVendor)
-    XCTAssertEqual(0, testVendorDataController.getAllEntities().count)
-    testVendorDataController.makeVendor()
-    XCTAssertEqual(1, testVendorDataController.getAllEntities().count)
+    SUT.vendorDataController.deleteEntityInContext((SUT.vendorDataController.MOC)!, entity: testVendor)
+    XCTAssertEqual(0, SUT.vendorDataController.getAllEntities().count)
+    SUT.vendorDataController.makeVendor()
+    XCTAssertEqual(1, SUT.vendorDataController.getAllEntities().count)
   }
   func testSessionCon()
   {
-    let vue = KVPrimeTVController()
-    let sessCon = vue.sessionDataController
-    sessCon.PSK = SUT_PSK!
-    sessCon.MOC = SUT_PSK!.viewContext
-    XCTAssertEqual(0, sessCon.getAllEntities().count, "Should be zero on  a fresh test")
-    sessCon.makeSession()
-    XCTAssertNotEqual(0, sessCon.getAllEntities().count, "Should be zero on  a fresh test")
-    let jiveSess = sessCon.getAllEntities().first
+    XCTAssertNotNil(SUT.sessions, "Nil Sessions")
+    XCTAssertEqual(0, SUT.sessionDataController.getAllEntities().count, "Should be zero on  a fresh test")
+    SUT.sessionDataController.makeSession()
+    XCTAssertNotEqual(0, SUT.sessionDataController.getAllEntities().count, "Should be zero on  a fresh test")
+    let jiveSess = SUT.sessionDataController.getAllEntities().first
     XCTAssertNotNil(jiveSess, "should be a session")
   }
-/**
-   OK What and why am I testing.
-If I look at the coverage now it is at 32% WHich on teh surface is a lot of untested code, but then again it is ALSO code that does't directly get tested. like the basic AppKit Functionality.
-*/
-  func testV13() {
-    let TVC = KVPrimeTVController()
-    TVC.AllDataController.PSK = SUT_PSK!
-    XCTAssertNotNil(TVC.AllDataController.MOC, "No MOC")
+  func testV13()
+  {
+//    let TVC = KVPrimeTVController()
+//    TVC.AllDataController.PSK = SUT_PSK!
+//    XCTAssertNotNil(TVC.AllDataController.MOC, "No MOC")
     
   }
-  func testPOne() {
-    //Corrected this context (ctx: (SUT_PSK?.viewContext)!) to what you see below ?
-    let TVC = KVPrimeTVController()
-    TVC.AllDataController.PSK = SUT_PSK!
-    let PDC = KVPersonDataController(ctx: (SUT_PSK!.viewContext))
+  func testPOne()
+  {
     for _ in 0 ... 1000 {
-      PDC.makePerson()
+      SUT.personDataController.makePerson()
     }
-    XCTAssertNotNil(TVC.AllDataController.MOC, "No MOC")
-    let  p:KVPerson = TVC.personDataController.createPersonInContext(SUT_PSK!.viewContext)
+    XCTAssertNotNil(SUT.AllDataController.MOC, "No MOC")
+    let  p:KVPerson = SUT.personDataController.createPersonInContext(PSK!.viewContext)
     XCTAssertNotNil(p, "nobody")
-    TVC.personDataController.resetPersonDefaults(p)
+    SUT.personDataController.resetPersonDefaults(p)
     XCTAssertEqual(maleNames[1], p.firstName, "Entity name should be the Default Michael")
     //
-    TVC.personDataController.setupRandomPerson(p)
+    SUT.personDataController.setupRandomPerson(p)
   }
   
 }
