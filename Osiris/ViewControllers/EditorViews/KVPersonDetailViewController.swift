f71e07add289eada147637cc1b9d966934db26c1
Kenn Villegas <kenn.@phoenix.64.local>
2017-08-28 14:08:22 -0400

    WIRED UP THE PERSON EDITOR TO THE XIB
    Added an editable person and configure textView is separate from
    configureView

--- Osiris/ViewControllers/EditorViews/KVPersonDetailViewController.swift
+++ Osiris/ViewControllers/EditorViews/KVPersonDetailViewController.swift
@@ -1,44 +1,81 @@
 /**
-  KVPersonDetailViewController.swift
-  Osiris
-
-  Created by Kenn Villegas on 7/18/17.
-  Copyright © 2017 dubian. All rights reserved.
-*/
+ KVPersonDetailViewController.swift
+ Osiris
+ 
+ Created by Kenn Villegas on 7/18/17.
+ Copyright © 2017 dubian. All rights reserved.
+ */
 
 import UIKit
 import MapKit
 
 class KVPersonDetailViewController: KVDetailViewController
 {
-  @IBOutlet weak var toolBarButton: UIBarButtonItem!
-  @IBOutlet weak var personsMapView: MKMapView!
-  @IBOutlet weak var personsNameTextField: UITextField!
-  @IBOutlet weak var midInitialTextField: UITextField!
-  @IBOutlet weak var personsAddressTextField: UITextField!
-  @IBOutlet weak var personsTypeTextField: UITextField!
-  @IBOutlet weak var currentFilterLabel: UILabel!
-  @IBOutlet weak var currentFilterTextField: UITextField!
+  /**
+  Yeah fixed these _BUT_ for some reason these were not at all wired to the GUI
+  */
+  @IBOutlet weak var toolBarButton = UIBarButtonItem()
+  @IBOutlet weak var personsMapView = MKMapView()
+  @IBOutlet weak var personsNameTextField = UITextField()
+  @IBOutlet weak var midInitialTextField = UITextField()
+  @IBOutlet weak var personsAddressTextField = UITextField()
+  @IBOutlet weak var personsTypeTextField = UITextField()
+  @IBOutlet weak var currentFilterLabel = UILabel()
+  @IBOutlet weak var currentFilterTextField = UITextField()
   
-  
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  var editablePerson: KVPerson? {
+    didSet {
+      configureView()
     }
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  }
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    configureView()
+  }
+  override func didReceiveMemoryWarning()
+  {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  func configureTextView()
+  {
+    if let p = editablePerson {
+      if let pTF = personsNameTextField
+      {
+//        let tName = p.firstName!
+        pTF.text = p.firstName!
+      }
+      if let mTF = midInitialTextField
+      {
+        mTF.text = ("¿-?") //p.middleName!
+      }
+      if let tTF = personsTypeTextField
+      {
+        tTF.text = p.type
+      }
+      if let aTF = personsAddressTextField
+      {
+        aTF.text = p.location?.address
+      }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+  }
+  func configureMapView() {
+    
+  }
+  // This is new
+  override func configureView() {
+    configureTextView()
+  }
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
 }
