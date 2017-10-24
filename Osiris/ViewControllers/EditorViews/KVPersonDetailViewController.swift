cb4fda0ecf6fd9b4df57286d56963aee3eba475d
Kenn Villegas <kenn.@phoenix.64.local>
2017-10-24 17:59:32 -0400

    DOCUMENTATION AND FORMATTING

--- Osiris/ViewControllers/EditorViews/KVPersonDetailViewController.swift
+++ Osiris/ViewControllers/EditorViews/KVPersonDetailViewController.swift
@@ -1,19 +1,21 @@
 /**
  KVPersonDetailViewController.swift
  Osiris
- 
+
  Created by Kenn Villegas on 7/18/17.
  Copyright © 2017 dubian. All rights reserved.
+
 
 */
 
 import UIKit
 import MapKit
-
-class KVPersonDetailViewController: KVMapViewController
+//Added UITextFieldDelegate; for
+class KVPersonDetailViewController: KVMapViewController, UITextFieldDelegate
 {
   /**
   Yeah fixed these _BUT_ for some reason these were not at all wired to the GUI
+ ¿is this true?
   */
   @IBOutlet weak var toolBarButton = UIBarButtonItem()
   @IBOutlet weak var personsMapView = MKMapView()
@@ -25,54 +27,61 @@ class KVPersonDetailViewController: KVMapViewController
   @IBOutlet weak var currentFilterTextField = UITextField()
   
 //  TODO: - Fix the sizes in the GUI
-  
+//  there is a trace for this in the debugger
+//Address button?
   var editablePerson: KVPerson? {
     didSet {
       configureView()
     }
   }
   
-  override func viewDidLoad()
-  {
+  override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     configureView()
   }
   
-  override func didReceiveMemoryWarning()
-  {
-    super.didReceiveMemoryWarning()
+  override func didReceiveMemoryWarning() {
     // Dispose of any resources that can be recreated.
+    super.didReceiveMemoryWarning()
   }
   
-  func configureTextView()
-  {
+  func configureTextView() {
+    personsNameTextField?.delegate = self
+    personsTypeTextField?.delegate = self
+    personsAddressTextField?.delegate = self
+    midInitialTextField?.delegate = self
+    currentFilterTextField?.delegate = self
+    personsNameTextField?.becomeFirstResponder()
+    /*
+    OKAY this is more like I was talking about in the MapViewExt
+    */
     if let p = editablePerson {
-      if let pTF = personsNameTextField
-      {
+      if let pTF = personsNameTextField {
 //        let tName = p.firstName!
+        if !(pTF.delegate === self) {
+          print("Delegate inactive???")
+        }
         pTF.text = p.firstName!
       }
-      if let mTF = midInitialTextField
-      {
+      if let mTF = midInitialTextField {
         mTF.text = ("¿-?") //p.middleName!
       }
-      if let tTF = personsTypeTextField
-      {
+      if let tTF = personsTypeTextField {
         tTF.text = p.type
       }
-      if let aTF = personsAddressTextField
-      {
+      if let aTF = personsAddressTextField {
         aTF.text = p.location?.address
       }
     }
-    
+
   }
   
   func configureMapView() {
-    
+    /**
+   
+    */
   }
-  
   // This is new
   override func configureView() {
     configureTextView()
